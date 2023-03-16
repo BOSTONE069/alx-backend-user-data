ba135aeb4d8ad0cf30a534a6a915852e3b7c2d3b
rhemney <bostoneochieng69@gmail.com>
2023-03-16 04:33:05 -0700

    adjusted the database to have the CRUD

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python3
-"""Database for the SQLALCHEMY"""
+""" Database for ORM """
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.orm import sessionmaker
+from typing import TypeVar
 from user import Base, User
 
 
 class DB:
-    """ DB class for the ORM"""
+    """ DB Class for Object Reational Mapping """
 
     def __init__(self):
-        """Constructor Method"""
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        """ Constructor Method """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self):
-        """Getter Session Method"""
+        """ Session Getter Method """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
@@ -47,9 +49,25 @@ class DB:
             if key not in column_names:
                 raise InvalidRequestError
 
-        user = self.__session.query(User).filter_by(**kwargs).first()
+        user = self._session.query(User).filter_by(**kwargs).first()
 
         if user is None:
             raise NoResultFound
 
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """ Update users attributes
+        Returns: None
+        """
+        user = self.find_user_by(id=user_id)
+
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise ValueError
+
+        for key, value in kwargs.items():
+            setattr(user, key, value)
+
+        self._session.commit()
